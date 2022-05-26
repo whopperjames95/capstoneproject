3e3af7dcaef948c504d3d8e644a431ca370f59b1
James Goff <james.goff1030@gmail.com>
2022-05-25 20:38:58 -0700

    Almost Finished

---
+++ home.js
@@ -0,0 +1,8 @@
+const image = document.querySelector('img');
+
+function click() {
+	alert("You have been awarded $75 OFF your next detail: YOUR COUPON CODE IS: Goffather1030, Schedule your detail Today, and provide coupon code! Expires in 30 minutes")
+}
+
+
+image.addEventListener("click", click)
---
+++ prices.js
@@ -0,0 +1,27 @@
+function myFunction() {
+  let popup = document.getElementById("extPopup");
+  popup.classList.toggle("show");
+}
+
+function myFunction2() {
+    let popup = document.getElementById("intPopup");
+    popup.classList.toggle("show");
+  }
+
+  function myFunction3() {
+    let popup = document.getElementById("fullPopup");
+    popup.classList.toggle("show");
+  }
+
+  function myFunction4() {
+    let popup = document.getElementById("ceramPopup");
+    popup.classList.toggle("show");
+  }
+
+  function myFunction5() {
+    let popup = document.getElementById("paintPopup");
+    popup.classList.toggle("show");
+  }
+
+
+
