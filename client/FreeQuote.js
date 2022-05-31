60ce92c6b877ef7d2e0d6d703051839e6893d23d
James Goff <james.goff1030@gmail.com>
2022-05-31 09:42:38 -0700

    Finished

--- client/FreeQuote.js
+++ client/FreeQuote.js
@@ -3,25 +3,28 @@
 
 function handlesubmit(event) {
     event.preventDefault()
-        const getName = document.querySelector('#name').value
-        const getEmail = document.querySelector('#email').value
-        const getMake = document.querySelector('#make').value
-        const getModel = document.querySelector('#model').value
+        const getName = document.querySelector('#name')
+        const getEmail = document.querySelector('#email')
+        const getMake = document.querySelector('#make')
+        const getModel = document.querySelector('#model')
     let body = {
-        getName: getName,
-        getEmail: getEmail,
-        getMake: getMake,
-        getModel: getModel
+        getName: getName.value,
+        getEmail: getEmail.value,
+        getMake: getMake.value,
+        getModel: getModel.value
     }
-    
+    console.log(body)
 
     axios
         .post('/api/userInfo', body)
-        .then (res => alert("Thank You! Someone Will Contact You Shortly....Don't Be Cheap."))
-    getName.textContent = ' ' 
-    getEmail.textContent = ' '
-    getMake.textContent = ' '
-    getModel.textContent = ' '
+        .then (res => {
+            alert("Thank You! Someone Will Contact You Shortly....Don't Be Cheap.")
+            getName.value = '' 
+            getEmail.value = ''
+            getMake.value = ''
+            getModel.value = ''
+        })
+
 }
 
 
